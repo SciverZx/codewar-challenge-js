f40ba416c5b9ad7eb2536dd8ea4d664f2168d7a2
SciverZx <anandaharysmk@gmail.com>
2024-02-16 20:10:32 +0800

    4/366

--- 4-smash.js
+++ 4-smash.js
@@ -1,6 +1,6 @@
-// WPU Coding Challenge 2024
-// day1 4/366 
-// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+// WPU Coding Challenge 2024 
+// day1 4/366  
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d 
 
 // function smash (words) {
 //     let result = words.join(' ');
