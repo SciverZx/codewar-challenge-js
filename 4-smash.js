1e979f45c5ecba8966f636ec8beb3233dfd546f0
SciverZx <anandaharysmk@gmail.com>
2024-02-16 20:07:48 +0800

    day1 4/366

---
+++ 4-smash.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// day1 4/366 
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// function smash (words) {
+//     let result = words.join(' ');
+//     return result;
+//  };
+
+const smash = (words) => words.join(' ');
+
+ console.log(smash(["hello","world","this","is","great"]));
