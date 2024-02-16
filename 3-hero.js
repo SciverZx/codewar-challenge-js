4a221aac74d3077651b3a97e0634f0f9661d6919
SciverZx <anandaharysmk@gmail.com>
2024-02-16 19:38:08 +0800

    update 3/366

--- 3-hero.js
+++ 3-hero.js
@@ -1,7 +1,14 @@
 // WPU Coding Challenge 2024
-// 1/366
+// 3/366
 // https://www.codewars.com/kata/59ca8246d751df55cc00014c
 
+// function hero(bullets, dragons) {
+//     if (bullets / 2 >= dragons) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 const hero = (bullets, dragons) => bullets/2 >= dragons;
 
