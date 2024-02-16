8a772e849ce779002515c00a127aee1474ab569d
SciverZx <anandaharysmk@gmail.com>
2024-02-16 22:29:33 +0800

    6/366

---
+++ 6-DNAtoRNA.js
@@ -0,0 +1,7 @@
+// WPU Coding Challenge 2024 
+// day1 6/366
+// https://www.codewars.com/kata/5556282156230d0e5e000089
+
+const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
+
+console.log(DNAtoRNA("GCAT"));
